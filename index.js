e7ecb3803b767c7227992ee0d24b865e7311ccd6
Adam Burnett <adam@thebur.net>
2017-12-17 20:51:17 -0500

    change api

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 var fs = require('fs');
 
  exports.get = function(event, context) {
-   var contents = fs.readFileSync("public/index.html");
+   var contents = {
+     "msg": "Hello, World"
+   };
    context.succeed({
      statusCode: 200,
      body: contents.toString(),
-     headers: {'Content-Type': 'text/html'}
+     headers: {'Content-Type': 'application/json'}
    });
  };
