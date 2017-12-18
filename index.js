209eda8cabc64bbcca75d81451787dd06aab7158
Adam Burnett <adam@thebur.net>
2017-12-17 21:32:54 -0500

    index

--- index.js
+++ index.js
@@ -2,12 +2,10 @@
 var fs = require('fs');
 
  exports.get = function(event, context) {
-   var contents = {
-     "msg": "Hello, World"
-   };
+   var contents = fs.readFileSync("public/index.html");
    context.succeed({
      statusCode: 200,
      body: contents.toString(),
-     headers: {'Content-Type': 'application/json'}
+     headers: {'Content-Type': 'text/html'}
    });
  };
